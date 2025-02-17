61d7f82f00f639c189113351d4ca0ed87eda7d78
zacharynarcotta <zachary.narcotta@maine.edu>
2025-02-17 15:15:52 -0500

    Creating ID and Caffeine variables for Patient class

--- Patient.java
+++ Patient.java
@@ -1,3 +1,9 @@
 public class Patient {
-    
+    public int id;
+    public double caffeine;
+
+    public Patient(int id, double caffeine) {
+        this.id = id;
+        this.caffeine = caffeine;
+    }
 }
