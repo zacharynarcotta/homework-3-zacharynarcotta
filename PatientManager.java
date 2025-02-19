a3a7d7785740240ba649066d55fe417b04dddbdb
zacharynarcotta <zachary.narcotta@maine.edu>
2025-02-19 12:20:21 -0500

    Creating PatientManager Class w/ necessary functions; Creating PatientTester instructions according to PDF outline

--- PatientManager.java
+++ PatientManager.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+
 public class PatientManager {
-    
+    public ArrayList<Patient> patientList;
+
+    public PatientManager() {
+        patientList = new ArrayList<Patient>();
+    }
+
+    public int addPatient(Patient patient) {
+        patientList.add(patient);
+        return patientList.indexOf(patient);
+    }
+
+    public Patient removePatient(int spot) {
+        Patient patient = patientList.get(spot);
+        patientList.remove(spot);
+        return patient;
+    }
+
+    public void caffeineAbsorption() {
+        for(int i = 0; i < patientList.size(); i++) {
+            Patient patient = patientList.get(i);
+            patient.caffeine = patient.caffeine - 160;
+            if(patient.caffeine <= 0) {
+                removePatient(i);
+                i--;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String toPrint = "";
+        if(patientList.size() == 0) {
+            toPrint = "Empty";
+        }
+        else {
+            for(int i = 0; i < patientList.size(); i++) {
+                Patient patient = patientList.get(i);
+                toPrint += patient.id + " " + patient.caffeine + "\n";
+            }
+        }
+        return toPrint;
+    }
 }
--- PatientTester.java
+++ PatientTester.java
@@ -1,3 +1,27 @@
 public class PatientTester {
-    
+    public static void main(String[] args) {
+        PatientManager manager = new PatientManager();
+        System.out.println(manager);
+
+        for(int i = 1; i < 5; i++) {
+            Patient patient = new Patient(i, 200 * i);
+            manager.addPatient(patient);
+        }
+        System.out.println(manager);
+
+        manager.caffeineAbsorption();
+        manager.caffeineAbsorption();
+        System.out.println(manager);
+
+        double highestCaffeine = manager.patientList.get(0).caffeine;
+        Patient patientToRemove = manager.patientList.get(0);
+        for(int i = 0; i < manager.patientList.size(); i++) {
+            Patient patient = manager.patientList.get(i);
+            if(patient.caffeine > highestCaffeine) {
+                patientToRemove = patient;
+            }
+        }
+        manager.removePatient(manager.patientList.indexOf(patientToRemove));
+        System.out.println(manager);
+    }
 }
